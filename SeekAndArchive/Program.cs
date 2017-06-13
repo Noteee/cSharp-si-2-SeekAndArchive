d8bd2aee13c80e10fa2278d3d0b7fb3840585b4c
Noteee <lugosi.norbert@gmail.com>
2017-06-13 14:39:34 +0200

    working without compression

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,56 @@ namespace SeekAndArchive
     {
         static void Main(string[] args)
         {
+            string nameOfTheDirectory = args[1];
+
+            string sourceDirectory = @nameOfTheDirectory;
+
+            string nameOfTheFile = args[0];
+
+            string filePath = searchFile();
+
+            string[] tokens = filePath.Split('\\');
+
+            string destinationPath = "C:\\Users\\lugos\\Desktop" + "\\" + "backup_" + tokens.Last();
+
+            string text = File.ReadAllText(@filePath);
+
+            string searchFile()
+            {
+                var fileList = new DirectoryInfo(sourceDirectory).GetFiles(nameOfTheFile, SearchOption.AllDirectories);
+                string searchedFileName = "";
+                foreach (var file in fileList)
+                {
+                    if (file.ToString() == nameOfTheFile)
+                    {
+                        searchedFileName = file.FullName;
+                    }
+                }
+                return searchedFileName;
+            }
+
+
+            byte[] GetFileHash(string fileName)
+            {
+                HashAlgorithm sha1 = HashAlgorithm.Create();
+                using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                    return sha1.ComputeHash(stream);
+            }
+
+            string hash = System.Text.Encoding.UTF8.GetString(GetFileHash(filePath));
+
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                if (hash != System.Text.Encoding.UTF8.GetString(GetFileHash(filePath)))
+                {
+                    hash = System.Text.Encoding.UTF8.GetString(GetFileHash(filePath));
+                    File.WriteAllText(destinationPath, text);
+                    text = File.ReadAllText(filePath);
+                }
+            }
         }
     }
 }
